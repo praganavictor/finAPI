cdd315a2002c1fdd76defd8887b13a4d0c1f7d6c
Victor Pragana <victormerigue@gmail.com>
2021-11-03 09:47:14 -0300

    Usuario pode criar uma conta

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists)
+    return res.status(400).json({ error: "Customer already exists" });
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.listen(3333);
