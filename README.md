8b7d1ff771f29341235047fba1966f53d8955c24
Victor Pragana <victormerigue@gmail.com>
2021-12-09 12:06:04 -0300

    withdraw method

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Trello da organização do projeto: https://trello.com/b/ZbLs6UoK/finapi
 
 [x] Deve ser possível realizar um depósito
 
-[] Deve ser possível realizar um saque
+[x] Deve ser possível realizar um saque
 
 [] Deve ser possível buscar o extrato bancário do cliente por data
 
@@ -26,7 +26,7 @@ Trello da organização do projeto: https://trello.com/b/ZbLs6UoK/finapi
 
 [x] Não deve ser possível fazer depósito em uma conta não existente
 
-[] Não deve ser possível fazer saque em uma conta não existente
+[x] Não deve ser possível fazer saque em uma conta não existente
 
 [] Não deve ser possível excluir uma conta não existente
 
--- src/index.js
+++ src/index.js
@@ -37,6 +37,17 @@ app.post("/account", (req, res) => {
     return res.status(201).send();
 });
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === "credit") {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+    return balance;
+}
+
 //app.use(verifyIfExistsAccountCPF);
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
@@ -55,6 +66,26 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
         amount,
         created_at: new Date(),
         type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) return response.status(400).json({ error: "Insufficient funds!" });
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
     };
 
     customer.statement.push(statementOperation);
